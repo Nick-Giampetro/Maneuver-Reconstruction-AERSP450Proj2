2d6da996018c645e5656628df92d34c1326037cb
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2023-11-08 18:36:11 -0500

    what i have done today need to study for 460 lol

--- AERSP450Project2.m
+++ AERSP450Project2.m
@@ -14,16 +14,15 @@ for i = 1:l
     LOS(i,3) = sind(ELEVATION(i)) ;
 end
 
-RSAT = zeros(l/3,3)
+RSAT = zeros(3,3,l/3) ;
 for i = 1:l/3
-    RSAT(i,:) = gauss(TIMES((i*3-2:i*3)) ,RSITES((i*3-2:i*3),:) ,LOS((i*3-2:i*3),:),MU)
+    RSAT(:,:,i) = gauss(TIMES((i*3-2:i*3)) ,RSITES((i*3-2:i*3),:) ,LOS((i*3-2:i*3),:),MU,Re)
     
 end 
 
 
 
-function Rsat = gauss(time,R,Phat,mu)
-    
+function Rsat = gauss(time,R,Phat,mu,Re)
     t1 = time(1) - time(2) ;
     t3 = time(3) - time(2) ;
     t  = time(3) - time(1) ;
@@ -46,11 +45,28 @@ function Rsat = gauss(time,R,Phat,mu)
     E = dot(R(2,:),Phat(2,:)) ;
     
     R2 = dot(R(2,:),R(2,:)) ;
-
     a = - (A^2 + 2*A*E + R2) ;
     b = - 2*mu*B*(A+E) ;
     c = - mu^2*B^2 ;
 
-    roots([1 0 a 0 0 b 0 0 c])
+    r2 = roots([1 0 a 0 0 b 0 0 c]) ;
+    r2 = r2(r2==real(r2)) ;
+    %rejects values that are not greater than the radius of the earth
+    r2 = r2(r2>Re) 
+    
+    rho(1) = 1/D0 * ( (6*(D(3,1)*t1/t3 + D(2,1)*t/t3) + mu*D(3,1)*(t^2-t1^2)*t1/t3) / (6*r2^3 + mu*(t^2-t3^2)) - D(1,1) ) ;
+    rho(2) = A + mu*B/r2^3 ;
+    rho(3) = 1/D0 * ( (6*(D(1,3)*t3/t1 + D(2,3)*t/t1) + mu*D(1,3)*(t^2-t1^3)*t3/t1) / (6*r2^3 + mu*(t^2-t1^2)) - D(3,3) ) ;
+
+    Rsat(1,:) = R(1,:) + rho(1) * p(1,:) ;
+    Rsat(2,:) = R(2,:) + rho(2) * p(2,:) ;
+    Rsat(3,:) = R(3,:) + rho(3) * p(3,:) 
+end
+
+
+
+function v2 = gibbs(r, mu)
+    
+    
 
 end
